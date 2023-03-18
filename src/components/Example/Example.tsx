457dbfdebb00dd89f381529bb9c722007a01a5ea
onejuice98 <waterpurifier@khu.ac.kr>
2023-03-18 17:19:44 +0900

    feat : card select

--- src/components/Example/Example.tsx
+++ src/components/Example/Example.tsx
@@ -1,4 +1,92 @@
+import React, { useState } from "react";
+import { randCardValue } from "../../lib/card";
+import GrayText from "../common/GrayText";
+import { CheckSVG } from "../common/svgs";
+
+let cardValues = randCardValue();
+
 const Example = () => {
-  return <div>THis is the Example</div>;
+  const [userPer, setUserPer] = useState<number>(0);
+  const [clickCnt, setClickCnt] = useState<number>(0);
+  const [selectedCard, setSelectedCard] = useState<number[]>([]);
+
+  const cardClicked = (value: number, index: number) => {
+    setClickCnt((prev) => prev + 1);
+    setUserPer((prev) => prev + value);
+    setSelectedCard([...selectedCard, index]);
+  };
+
+  const initialBtn = () => {
+    setClickCnt(0);
+    setUserPer(0);
+    setSelectedCard([]);
+    cardValues = randCardValue();
+  };
+
+  const deleteSelectedCard = (index: number) => {
+    let copyed = selectedCard;
+    setSelectedCard(copyed.filter((_, idx) => idx !== index));
+    setClickCnt((prev) => prev - 1);
+  };
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex justify-between gap-4">
+        <div className="flex flex-col w-full p-4 bg-white rounded-md shadow-md">
+          <span className="w-full py-2 text-2xl border-b">모의기 사용방법</span>
+        </div>
+      </div>
+
+      <div className="flex flex-wrap gap-4">
+        {cardValues.map((value, index) => (
+          <button
+            key={index}
+            onClick={() => cardClicked(value, index)}
+            value={value}
+            disabled={selectedCard.includes(index)}
+            className="flex items-center justify-center w-32 h-32 bg-white rounded-md shadow-md font-medium text-2xl hover:bg-emerald-100 hover:scale-110 duration-500"
+          >
+            {selectedCard.includes(index) && (
+              <div className="absolute w-32 h-32 rounded-md">
+                <CheckSVG w={32} h={32} />
+              </div>
+            )}
+            {index + 1}
+          </button>
+        ))}
+      </div>
+      <div className="grid grid-cols-2 gap-2 whitespace-nowrap">
+        <div className="flex flex-col justify-between bg-white rounded-md shadow-md p-2 gap-2">
+          <div className="flex gap-2">
+            <GrayText>선택된 카드 :</GrayText>
+
+            {selectedCard.map((num, idx) => (
+              <span
+                key={idx}
+                onClick={() => deleteSelectedCard(idx)}
+                className="flex justify-center items-center font-medium w-8 bg-green-300 rounded-md hover:bg-green-700"
+              >
+                {num + 1}
+              </span>
+            ))}
+          </div>
+          <span className="text-gray-400 text-sm">
+            * 초록 숫자를 눌러 선택한 카드를 제거할 수 있습니다.
+          </span>
+        </div>
+        <div className="flex items-center bg-white rounded-md shadow-md p-4 gap-2">
+          <GrayText>결과 :</GrayText>
+          <span className="font-medium text-lg">
+            {clickCnt === 4 && userPer / 4}
+          </span>
+        </div>
+      </div>
+      <button
+        className="whitespace-nowrap text-lg font-medium text-white p-4 bg-red-500 rounded-md hover:bg-red-700"
+        onClick={initialBtn}
+      >
+        초기화
+      </button>
+    </div>
+  );
 };
 export default Example;
---
+++ src/lib/card.ts
@@ -0,0 +1,97 @@
+const CARD_COUNT = 15;
+const OFFSET_VALUES = [
+  { min: -0.2269, max: -0.135 },
+  { min: -0.6928, max: -0.4529 },
+  { min: -1.0979, max: -0.833 },
+  { min: -1.4929, max: -1.176 },
+  { min: -1.837, max: -1.5369 },
+  { min: -2.2289, max: -1.9199 },
+  { min: -2.567, max: -2.3 },
+  { min: -2.9189, max: -2.7099 },
+  { min: 0.042, max: 0.4141 },
+  { min: 0.438, max: 0.815 },
+  { min: 0.871, max: 1.2151 },
+  { min: 1.2921, max: 1.697 },
+  { min: 1.7362, max: 2.086 },
+  { min: 2.173, max: 2.527 },
+  { min: 2.663, max: 2.877 },
+];
+
+const offsetRand = (min: number, max: number) => {
+  return Math.random() * (max - min) + min;
+};
+
+// 피셔-예이츠 셔플 알고리즘
+const shuffle = (array: number[]) => {
+  for (let index = array.length - 1; index > 0; index--) {
+    const randomPosition = Math.floor(Math.random() * (index + 1));
+
+    const temp = array[index];
+    array[index] = array[randomPosition];
+    array[randomPosition] = temp;
+  }
+  return array;
+};
+
+export const randCardValue = () => {
+  let cardValues: number[] = [];
+  for (let i = 0; i < CARD_COUNT; i++) {
+    cardValues.push(offsetRand(OFFSET_VALUES[i].min, OFFSET_VALUES[i].max));
+  }
+  return shuffle(cardValues);
+};
+/*
+const test = [
+    {
+      target: 170130000,
+      tests: [
+        170201500, 170960300, 169786400, 173279200, 168712900, 165164000,
+        172945700, 167744800, 169318500, 166337900, 167515200, 174994100,
+        174429200, 165784900, 171611900,
+      ],
+    },
+    {
+      target: 168576000,
+      tests: [
+        164248700, 166593600, 172043700, 169274000, 168335000, 172611800,
+        165951300, 167530900, 171436800, 173147800, 163945300, 169314400,
+        164928100, 167148200, 170046000,
+      ],
+    },
+    {
+      target: 99240000,
+      tests: [
+        98356800, 97983700, 97417000, 96436500, 99447500, 100298900, 101017400,
+        101882800, 96843400, 99035600, 100789200, 97233400, 101651600, 98790500,
+        99866300,
+      ],
+    },
+    {
+      target: 40300000,
+      tests: [
+        41283800, 39207900, 39607300, 40245600, 40392700, 40999700, 40592600,
+        40652700, 39725400, 39950200, 40020800, 40820700, 39526300, 41384100,
+        39373100,
+      ],
+    },
+    {
+      target: 68720000,
+      tests: [
+        67694100, 68564100, 69750200, 68312500, 70153500, 66735400, 69555000,
+        67965500, 67114100, 68852700, 69280100, 67362100, 67514700, 70213300,
+        70697100,
+      ],
+    },
+  ];
+
+  let result: any = [];
+  const results = test.map((value) => {
+    let temp: number[] = [];
+    value.tests.map((price) => {
+      temp.push(((price - value.target) / value.target) * 100);
+    });
+    temp.sort();
+    result.push(temp);
+
+  });
+  */
