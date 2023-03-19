dba73b00b71e196722f7a2de3122e737ef244c52
onejuice98 <waterpurifier@khu.ac.kr>
2023-03-19 15:09:09 +0900

    new repo..,,

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,8 +2,11 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware("/api", {
+    createProxyMiddleware("/najangbot2/api/posts", {
       target: "https://g2b-bot.herokuapp.com",
+      pathRewrite: {
+        "^/najangbot2": "",
+      },
       changeOrigin: true,
     })
   );
