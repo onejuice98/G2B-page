52eef27c6beb30e67d95ca3029b4347f1bb9b700
onejuice98 <waterpurifier@khu.ac.kr>
2023-04-01 23:05:51 +0900

    feat : detail style

--- src/components/List/Detail.tsx
+++ src/components/List/Detail.tsx
@@ -7,26 +7,34 @@ interface DetailProps {
 const Detail = ({ data, loading }: DetailProps) => {
   return (
     <>
-      {!loading && (
+      {!loading && data.length > 0 && (
         <div className="w-full p-4 bg-white rounded-md shadow-md">
-          <table>
-            <thead>
-              <tr>
+          <div className="text-gray-500">
+            <span> 기초금액 : {data[0].기초금액} 원 </span>
+          </div>
+          <table className="w-full table-fixed border">
+            <thead className="border-b-2 text-gray-600">
+              <tr className="border">
                 <th>업체명</th>
                 <th>대표자명</th>
                 <th>입찰금액(원)</th>
                 <th>투찰률(%)</th>
-                <th>기초금액</th>
               </tr>
             </thead>
-            <tbody>
+            <tbody className="text-center">
               {data.map((value, index) => (
-                <tr key={index}>
-                  <td> {value.업체명}</td>
-                  <td> {value.대표자명}</td>
-                  <td> {value["입찰금액(원)"]}</td>
-                  <td> {value["투찰률(%)"]}</td>
-                  <td> {value.기초금액}</td>
+                <tr
+                  key={index}
+                  className={`${
+                    index % 2 === 0 && `bg-gray-200`
+                  } text-gray-600 p-1`}
+                >
+                  <td className="hover:text-green-500">{value.업체명}</td>
+                  <td className="hover:text-green-500">{value.대표자명}</td>
+                  <td className="hover:text-green-500">
+                    {value["입찰금액(원)"]}
+                  </td>
+                  <td className="hover:text-green-500">{value["투찰률(%)"]}</td>
                 </tr>
               ))}
             </tbody>
