9dcaa189559f97aa32578e793ac2547653ab8728
onejuice98 <waterpurifier@khu.ac.kr>
2023-04-05 15:18:10 +0900

    feat : conflick 해결

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,4 +1,3 @@
-import { AppBar, Avatar, Box, Toolbar } from "@mui/material";
 import { BellSVG, SettingSVG } from "../common/svgs";
 
 interface HeaderProps {
@@ -6,26 +5,20 @@ interface HeaderProps {
 }
 const Header = ({ current }: HeaderProps) => {
   return (
-    <Box>
-      {/* Logo */}
-      <Box sx={{ width: 228, display: flex }}></Box>
-    </Box>
-
-    // <header className="fixed w-full max-h-[80px] z-20 bg-white">
-    //   <nav className="flex items-center w-full h-16 gap-2 py-4">
-    //     <div className="w-40 flex justify-center items-center">
-    //       <span className="font-bold text-3xl text-green-400">👾 나장봇</span>
-    //     </div>
-    //     <div className="flex items-center gap-2">
-    //       <span className="font-semibold text-2xl text-gray-700">
-    //         {current}
-    //       </span>
-    //       <BellSVG w={20} h={20} />
-    //       <SettingSVG w={20} h={20} />
-    //       <Avatar alt="Juice Won" sx={{ width: 28, height: 28 }} />
-    //     </div>
-    //   </nav>
-    // </header>
+    <header className="fixed w-full max-h-[80px] z-20 bg-white">
+      <nav className="flex items-center w-full h-16 gap-2 py-4">
+        <div className="w-40 flex justify-center items-center">
+          <span className="font-bold text-3xl text-green-400">👾 나장봇</span>
+        </div>
+        <div className="flex items-center gap-2">
+          <span className="font-semibold text-2xl text-gray-700">
+            {current}
+          </span>
+          <BellSVG w={20} h={20} />
+          <SettingSVG w={20} h={20} />
+        </div>
+      </nav>
+    </header>
   );
 };
 export default Header;
